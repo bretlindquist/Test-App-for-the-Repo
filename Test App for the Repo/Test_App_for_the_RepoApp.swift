353c1b8947e67773106b8d481c030d6dbb8429d8
Bret Lindquist <30852056+bretlindquist@users.noreply.github.com>
2024-02-01 21:01:56 +0900

    Initial Commit

---
+++ Test App for the Repo/Test_App_for_the_RepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_App_for_the_RepoApp.swift
+//  Test App for the Repo
+//
+//  Created by Bret Allan Lindquist on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_App_for_the_RepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
