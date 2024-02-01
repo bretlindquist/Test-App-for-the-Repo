ddcc6c432d26744c3932c266edb74dec733223d1
Bret Lindquist <30852056+bretlindquist@users.noreply.github.com>
2024-02-01 21:47:50 +0900

    Added Menu List Items

--- Test App for the Repo/ContentView.swift
+++ Test App for the Repo/ContentView.swift
@@ -8,31 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-@State private var showMenu = false
+  @State private var showMenu = false
   
   var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .navigationTitle("Title")
-        .navigationBarTitleDisplayMode(.inline)
-        .toolbar {
-          ToolbarItem(placement: .navigationBarLeading) {
-            Button { showMenu.toggle() }
-          label: {
+    NavigationStack {
+      List {
+        NavigationLink("Mint", value: Color.mint)
+        NavigationLink("Pink", value: Color.pink)
+        NavigationLink("Teal", value: Color.teal)
+      }
+      .navigationDestination(for: Color.self) { color in
+        Text(color.description)
+      }
+      .navigationTitle("Title")
+      .navigationBarTitleDisplayMode(.inline)
+      .toolbar {
+        ToolbarItem(placement: .navigationBarLeading) {
+          Button { showMenu.toggle() } label: {
             Image(systemName: "line.3.horizontal")
           }
-          }
         }
-        .padding()
+      }
+      
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        AppTabView()
     }
 }
